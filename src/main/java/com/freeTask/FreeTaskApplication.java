42c4984447a1621332f3d5cd6636eafcbe7e1aee
w.k <tekno753@gmail.com>
2022-04-29 12:36:11 +0900

    init

---
+++ src/main/java/com/freeTask/FreeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.freeTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freeTask/FreeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
